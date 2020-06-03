81956e752e336f06952d122e1d55a894bae4b387
Quastrado <you@example.com>
2020-06-03 15:43:25 +0300

    sending picture by keyboard

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import logging
 from random import choice, randint
 
 from emoji import emojize
+from telegram import ReplyKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 import settings
@@ -16,14 +17,19 @@ PROXY = {'proxy_url': settings.PROXY_URL,
 def greet_user(update, context):
     print('Start triggered')
     context.user_data['emoji'] = get_smile(context.user_data)
-    update.message.reply_text(f'Hey!{context.user_data["emoji"]}')
+    update.message.reply_text(
+        f'Hey!{context.user_data["emoji"]}',
+        reply_markup=main_keyboard()
+        )
 
 
 def talk_to_me(update, context):
     context.user_data['emoji'] = get_smile(context.user_data)
     text = update.message.text
     print(text)
-    update.message.reply_text(f'{text} {context.user_data["emoji"]}')
+    update.message.reply_text(
+        f'{text} {context.user_data["emoji"]}',
+        reply_markup=main_keyboard())
 
 
 def get_smile(user_data):
@@ -31,6 +37,7 @@ def get_smile(user_data):
         smile = choice(settings.USER_EMOJI)
         return emojize(smile, use_aliases=True)
     return user_data['emoji']
+
 
 def play_random_numbers(user_number):
     bot_number = randint(user_number-10, user_number+10)
@@ -52,14 +59,25 @@ def guess_number(update, context):
             message = 'Enter an integer'
     else:
         message = 'Enter the number'
-    update.message.reply_text(message)
+    update.message.reply_text(
+        message,
+        reply_markup=main_keyboard()
+        )
 
 
 def send_picture(update, context):
     picture_list = glob('images/pic*.jpg')
     picture = choice(picture_list)
     chat_id = update.effective_chat.id
-    context.bot.send_photo(chat_id=chat_id, photo=open(picture, 'rb')) 
+    context.bot.send_photo(
+        chat_id=chat_id,
+        photo=open(picture, 'rb'),
+        reply_markup=main_keyboard()
+        ) 
+
+
+def main_keyboard():
+    return ReplyKeyboardMarkup([['Image']])
 
 
 def main():
@@ -68,6 +86,7 @@ def main():
     dp.add_handler(CommandHandler('start', greet_user))
     dp.add_handler(CommandHandler('guess', guess_number))
     dp.add_handler(CommandHandler('pic', send_picture))
+    dp.add_handler(MessageHandler(Filters.regex('^(Image)$'), send_picture))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
     logging.info('LazyBoy was start')
     mybot.start_polling()
